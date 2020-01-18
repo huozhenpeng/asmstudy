9eaa1ee008acc21d34e3a3fd2891f43237cae722
霍振鹏 <huozhenpengdyx@gmail.com>
2020-01-18 11:07:09 +0800

    测试静态方法

---
+++ app/src/main/java/com/miduo/asmstudy/SingleTon.java
@@ -0,0 +1,13 @@
+package com.miduo.asmstudy;
+
+public class SingleTon {
+
+    public static void  doSomeThing()
+    {
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
